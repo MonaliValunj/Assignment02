e39548259f4ba6e883798e8789d5e91a157a147d
Monali Valunj <monavalunj96@gmail.com>
2023-10-02 23:10:43 -0700

    All solved

--- js/04-grade-assigner.js
+++ js/04-grade-assigner.js
@@ -1,5 +1,17 @@
 let score = parseInt(prompt("Enter score between 1 and 100 : "))
 
-if(score < 1 && score >100){
-    score = parseInt(prompt("Enter score again : "))
+if(score < 1 || score > 100){
+    document.write("only numbers between 1 and 100 are accepted")
+}else{
+    if(score >= 90){
+        console.log("You received an A")
+    }else if(score >= 80){
+        console.log("You received a B")
+    } else if(score >= 70){
+        console.log("You received a C")
+    } else  if(score >= 60){
+        console.log("You received a D")
+    }else {
+        console.log("You received an F")
+    }
 }
